d531b02bef4d585d4396a89a4506ce22e14614c2
root <root@vmi1484973.contaboserver.net>
2024-06-06 13:20:21 +0200

    nothing

--- execve_utils.c
+++ execve_utils.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 20:15:32 by loic              #+#    #+#             */
-/*   Updated: 2024/05/24 15:38:50 by root             ###   ########.fr       */
+/*   Updated: 2024/05/30 16:52:20 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,31 @@
 // 	argument = ft_split(user_input, ' ');
 // 	//exec_command(char *command, char** argument, char **envp);
 // }
-
-void	check_path(char *cmd)
-{
-	int		counter;
-	char	*path; 
-	char	**splitted_path;
+// void	display_envp(char **envp)
+// {
 	
-	counter = 0;
-   	path = getenv("PATH");
-	splitted_path = ft_split(path, ':');
-	while (splitted_path[counter])
-	{
-		//printf("%s\n", access());
-		counter++;
-	}
-}
+// }
+
+// void	check_path(char *cmd)
+// {
+// 	int		counter;
+// 	char	*path; 
+// 	char	**splitted_path;
+	
+// 	counter = 0;
+//    	path =  ("PATH");
+// 	splitted_path = ft_split(path, ':');
+// 	while (splitted_path[counter])
+// 	{
+// 		//printf("%s\n", access());
+// 		counter++;
+// 	}
+// }
 int		exec_command(char *command, char** argument, char **envp)
 {
 	char *command_path;
-
 	command_path = "/bin/";
-	check_path(command);
+	//check_path(command);
 	// Maybe use strncat
 	command_path = ft_strjoin(command_path, command);
 	debug_display_array(argument);
